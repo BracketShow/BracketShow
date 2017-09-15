f7ff1637dc94705a02c0c7fe2da7f3a14e20bf77
Eric De Carufel <edecarufel@pyxis-tech.com>
2017-09-15 09:00:46 -0400

    Use image from channel 9

--- src/BracketShow/Models/HomeViewModels/VideoViewModel.cs
+++ src/BracketShow/Models/HomeViewModels/VideoViewModel.cs
@@ -5,11 +5,23 @@ namespace Models.HomeViewModels
 {
     public class VideoViewModel
     {
+        private string _image;
         public string YouTubeId => YouTubeUrl.Replace("https://www.youtube.com/watch?v=", "");
 
         public string YouTubeUrl { get; set; }
         public string Category { get; set; }
         public string Title { get; set; }
         public DateTime PublishDate { get; set; }
+
+        public string Image
+        {
+            get
+            {
+                if (_image == null)
+                    return String.Format("http://img.youtube.com/vi/{0}/0.jpg", YouTubeId);
+                return _image;
+            }
+            set { _image = value; }
+        }
     }
 }
